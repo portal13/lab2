b3311fb111337540382717519bfb5ceea274f7ac
portal <nevlasenko@gmail.com>
2015-03-15 23:32:10 +0200

    functions for my lab

--- functions.py
+++ functions.py
@@ -1,2 +1,26 @@
 import pandas as pd
 
+def read_and_filter(fileName):
+    df = pd.read_csv("downloads/%s"%fileName, index_col=False, header=1)
+    df = df[['year','week','VCI','TCI','VHI','%Area_VHI_LESS_15','%Area_VHI_LESS_35']]
+    df = df[(df['VHI']>=0)]
+    return df
+
+def max_VHI(df, year):
+    yr=df[df['year']==int(year)] #max VHI
+    max1 = max(yr.VHI)
+    return max1
+
+def min_VHI(df, year):
+    yr=df[df['year']==int(year)] #min VHI
+    min1 = min(yr.VHI)
+    return min1
+
+def extreme_drought_area(df, percent):
+    a=df[(df['%Area_VHI_LESS_15']>=int(percent))] #search for drought
+    return a
+
+def moderate_drought_area(df, percent):
+    a=df[(df['%Area_VHI_LESS_35']>=int(percent))] #search for drought
+    return a
+    
